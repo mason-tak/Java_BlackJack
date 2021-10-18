e769324ef8badcae7b81f8eb5eb5152e93fa2d98
SeungHyeonTak-sub <conficker77@gmail.com>
2021-10-18 14:05:30 +0900

    - 게임에 필요한 변수 및 메서드 생성

---
+++ src/com/blackjack/person/Gamer.java
@@ -0,0 +1,18 @@
+package com.blackjack.person;
+
+import com.blackjack.cards.Card;
+
+import java.util.List;
+
+// 게이머
+public class Gamer {
+    private List<Card> cards;
+
+    public void receiveCard(Card card) {
+
+    }
+
+    public List<Card> openCards() {
+        return null;
+    }
+}
---
+++ src/com/blackjack/rules/Rule.java
@@ -0,0 +1,18 @@
+package com.blackjack.rules;
+
+import com.blackjack.cards.Card;
+import com.blackjack.person.Dealer;
+import com.blackjack.person.Gamer;
+
+import java.util.List;
+
+// 규칙
+public class Rule {
+    public int getScore(List<Card> cards) {
+        return 0;
+    }
+
+    public void getWinner(Dealer dealer, Gamer gamer) {
+
+    }
+}
