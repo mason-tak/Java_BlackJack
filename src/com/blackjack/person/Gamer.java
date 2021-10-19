db021017a4e356408a99218590424d756fecb58c
SeungHyeonTak-sub <conficker77@gmail.com>
2021-10-19 22:56:01 +0900

    - commit 작성   - Gammer class     - 뽑은 카드를 저장할 리스트 생성 (ArrayList)     - receiveCard() 메서드를 통해서 뽑은 카드 저장     - showCards() 메서드를 통해서 현재 보유하고 있는 카드 리스트 호출   - Gamer class     - playingPhase()       - 카드를 gamer에게 전달하는 기능       - Gamer class의 receiverCard와 showCards를 호출     - initPhase()       - 게임 시작시 고정적으로 받는 2장의 카드를 받는 기능       - playingPhase() 메서드와 기능적으로는 같다.

--- src/com/blackjack/person/Gamer.java
+++ src/com/blackjack/person/Gamer.java
@@ -2,17 +2,41 @@ package com.blackjack.person;
 
 import com.blackjack.cards.Card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 게이머
 public class Gamer {
     private List<Card> cards;
+    int numberGames = 0;
+
+    public Gamer() {
+        cards = new ArrayList<>();  // 뽑은 카드를 담을 곳
+    }
 
     public void receiveCard(Card card) {
+        // 뽑은 카드 소유
+        this.cards.add(card);
+        // 카드를 받을때 현재 소유한 카드를 확인해야함
+        this.showCards();
+    }
 
+    public void showCards() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재 보유 카드 목록 \n");
+
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+        if (numberGames >= 1) System.out.println(sb.toString());
+
+        numberGames++;
     }
 
     public List<Card> openCards() {
-        return null;
+        // 카드 오픈
+        // 현재 가지고 있는 모든 카드들을 전달하는 역할
+        return this.cards;
     }
 }
