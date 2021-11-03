9b99d885939b35d06750f7cc6a03ccf535bd91b3
SeungHyeonTak-sub <conficker77@gmail.com>
2021-11-03 23:31:50 +0900

    - dealer 중간 commit

--- src/com/blackjack/cards/Card.java
+++ src/com/blackjack/cards/Card.java
@@ -4,10 +4,37 @@ package com.blackjack.cards;
 public class Card {
     private String pattern;  // 무늬
     private String denomination;  // 끗수 (A, 2~10, J,Q,K)
+    private int point;
 
-    public Card(String pattern, String denomination) {
+    public Card(String pattern, String denomination, int point) {
         this.pattern = pattern;
         this.denomination = denomination;
+        this.point = point;
+    }
+
+    public Card(String pattern, int index) {
+        this.pattern = pattern;
+        this.denomination = this.numberToDenomination(index);
+        this.point = this.numberToPoint(index);
+    }
+
+    private String numberToDenomination(int number) {
+        if (number == 1) {
+            return "A";
+        } else if (number == 11) {
+            return "J";
+        } else if (number == 12) {
+            return "Q";
+        } else if (number == 13) {
+            return "K";
+        }
+
+        return String.valueOf(number);
+    }
+
+    private int numberToPoint(int number) {
+        if (number >= 11) return 10;
+        return number;
     }
 
     public String getPattern() {
@@ -24,6 +51,38 @@ public class Card {
 
     public void setDenomination(String denomination) {
         this.denomination = denomination;
+    }
+
+    public enum Denomination {
+        ACE("A", 1),
+        TWO("2", 2),
+        THREE("3", 3),
+        FOUR("4", 4),
+        FIVE("5", 5),
+        SIX("6", 6),
+        SEVEN("7", 7),
+        EIGHT("8", 8),
+        NINE("9", 9),
+        TEN("10", 10),
+        JACK("J", 10),
+        QUEEN("Q", 10),
+        KING("K", 10);
+
+        private String mark;
+        private int point;
+
+        Denomination() {
+
+        }
+
+        Denomination(String mark, int point) {
+            this.mark = mark;
+            this.point = point;
+        }
+
+        public int getPoint() {
+            return this.point;
+        }
     }
 
     @Override
